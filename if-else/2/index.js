b03ddc91c269c7dd26b72d19ca1ccdb531e6be33
Shruti Shende <shrutishende11@gmail.com>
2024-02-01 21:14:04 +0530

    program to find maximum between three numbers.

---
+++ if-else/2/index.js
@@ -0,0 +1,11 @@
+const num1 = 10
+const num2 = 40
+const num3 = 30
+
+if(num1>num2 && num1 > num3){
+    console.log(`${num1} is max num`)
+}else if(num2>num3) {
+    console.log(`${num2} is max num`)
+}else{
+    console.log(`${num3} is max num`)
+}
