a51bc0b1f9899be561a18f50127dc4808f7ee97c
Shruti Shende <shrutishende11@gmail.com>
2024-02-02 14:36:10 +0530

    program to enter week number and print day of week.

---
+++ if-else/11/index.js
@@ -0,0 +1,18 @@
+const week = "5"
+
+
+if (week === "1"){
+    console.log("Monday")
+} else if(week ==="2"){
+    console.log("Tuesday")
+}else if (week === "3"){
+    console.log("Wednesday")
+}else if (week === "4"){
+    console.log("Thursday")
+}else if( week === "5"){
+    console.log("Friday")
+}else if(week === "6"){
+    console.log("Saturday")
+}else{
+    console.log("sunday")
+}
