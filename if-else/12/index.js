02f8685f5cdc02219c9ee505def7b4376dff2b30
Shruti Shende <shrutishende11@gmail.com>
2024-02-02 15:19:50 +0530

    program to find number of days in month.

---
+++ if-else/12/index.js
@@ -0,0 +1,27 @@
+const month = "2"
+
+if (month === "1"){
+    console.log("31 days")
+}else if (month === "2"){
+    console.log("28/29 days")
+}else if (month === "3"){
+    console.log("31 days")
+}else if (month === "4"){
+    console.log("30 days")
+}else if (month === "5"){
+    console.log("31 days")
+}else if (month === "6"){
+    console.log("30 days")
+}else if (month === "7"){
+    console.log("31 days")
+}else if (month === "8"){
+    console.log("31 days")
+}else if (month === "9"){
+    console.log("30 days")
+}else if (month === "10"){
+    console.log("31 days")
+}else if (month === "11"){
+    console.log("30 days")
+}else if (month === "12"){
+    console.log("31 days")
+}
