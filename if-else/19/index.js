10432f511b3c2984b2a4fb235142120a84bf788d
Shruti Shende <shrutishende11@gmail.com>
2024-02-07 13:08:39 +0530

    program to enter student marks and find percentage and grade.

---
+++ if-else/19/index.js
@@ -0,0 +1,27 @@
+const sub1 =95
+const sub2 =95
+const sub3=97
+const sub4=98
+const sub5 =90
+
+
+const percent = (sub1+sub2+sub3+sub4+sub5) *100/500
+console.log(percent)
+if(percent>= 90){
+    console.log("grade A")
+} else if(percent>=80){
+    console.log("grade B")
+
+}else if(percent>=70){
+    console.log("grade C")
+
+}else if(percent>=60){
+    console.log("grade D")
+
+}else if(percent>=40){
+    console.log("grade E")
+
+}else{
+    console.log("grade F")
+}
+
