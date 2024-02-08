bc96cfae27ccc6224b6656e3769e11af370a85cc
Shruti Shende <shrutishende11@gmail.com>
2024-02-08 23:02:36 +0530

    program to print number of days in a month using switch case.

---
+++ switch-cases/2/index.js
@@ -0,0 +1,40 @@
+const month = 3
+
+switch (month){
+    case 1:
+        console.log("31 days")
+        break;
+    case 2 :
+        console.log("28/29 days")
+        break
+    case 3:
+        console.log("31 days")
+        break;
+    case 4:
+        console.log("30 days")
+        break;
+    case 5:
+        console.log("31 days")
+        break;
+    case 6:
+        console.log("30 days")
+        break;
+    case 7:
+        console.log("31 days")
+        break;
+    case 8:
+        console.log("31 days")
+        break;
+    case 9:
+        console.log("30 days")
+        break;
+    case 10:
+        console.log("31 days")
+        break;
+    case 11:
+        console.log("30 days")
+        break;
+    case 12:
+        console.log("31 days")
+        break;
+}
