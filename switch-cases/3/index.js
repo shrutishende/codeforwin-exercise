93f03c8dd9d49e6446d9eb3f612546716a64d2b6
Shruti Shende <shrutishende11@gmail.com>
2024-02-08 23:44:59 +0530

    program to check vowel or consonant using switch case.

---
+++ switch-cases/3/index.js
@@ -0,0 +1,36 @@
+const alphabet = "i"
+
+switch (alphabet){
+    case "a":
+        console.log("vowel")
+        break
+    case "e":
+        console.log("vowel")
+        break
+    case "i":
+        console.log("vowel")
+        break
+    case "o":
+        console.log("vowel")
+        break
+    case "u":
+        console.log("vowel")
+        break
+    case "A":
+        console.log("vowel")
+        break
+    case "E":
+        console.log("vowel")
+        break
+    case "I":
+        console.log("vowel")
+        break
+    case "O":
+        console.log("vowel")
+        break
+    case "U":
+        console.log("vowel")
+        break
+    default:
+        console.log("consonant")
+}
