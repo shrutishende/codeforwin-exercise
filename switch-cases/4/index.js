61a0688af2aa54ff3a2a82d359f857a87236107f
Shruti Shende <shrutishende11@gmail.com>
2024-02-08 23:56:47 +0530

    program to find maximum between two numbers using switch case Categories.

---
+++ switch-cases/4/index.js
@@ -0,0 +1,11 @@
+const num1 = 19
+const num2 = 12
+
+switch (num1> num2){
+    case true:
+        console.log(`max num : ${num1}` )
+        break
+    case false:
+        console.log(`max num : ${num2}` )
+        break
+}
