c89b25025322b71d40daae0219dbb40be76920f3
Shruti Shende <shrutishende11@gmail.com>
2024-02-12 12:43:07 +0530

    program to create calculator using switch case and functions.

---
+++ switch-cases/8/index.js
@@ -0,0 +1,25 @@
+const num1 = 5
+const num2 = 3
+const operator = "*"
+
+let result
+
+switch (operator){
+    case "-":
+     result =  num1 - num2
+        console.log(result)
+        break
+    case "+":
+         result = num1 + num2
+        console.log(result)
+        break
+    case "*":
+        result = num1 * num2
+        console.log(result)
+        break
+    case "/":
+        result = num1 / num2
+        console.log(result)
+        break
+
+}
