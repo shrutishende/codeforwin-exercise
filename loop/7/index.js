774b28c8d736b558f5d798495022f1f62258bb86
Shruti Shende <shrutishende11@gmail.com>
2024-02-13 14:16:40 +0530

    program to find sum of even numbers between 1 to n.

---
+++ loop/7/index.js
@@ -0,0 +1,18 @@
+// let sum =0
+// for (let i=1; i<=10; i++){
+//     if(i%2===0){
+//        sum = sum + i
+//     }
+// }
+// console.log(sum
+//  )
+
+let sum =0
+let i=1
+while(i<=10){
+    if(i%2===0){
+        sum = sum +i
+    }
+    i++
+}
+console.log(sum)
