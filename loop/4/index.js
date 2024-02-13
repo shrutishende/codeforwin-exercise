8b573e68277e2e07075c16c1214de1df9563f8ae
Shruti Shende <shrutishende11@gmail.com>
2024-02-13 13:24:48 +0530

    program to print all even numbers from 1 to n.

---
+++ loop/4/index.js
@@ -0,0 +1,14 @@
+// for(let i = 1 ; i<=10; i++){
+//     if(i%2===0){
+//         console.log(i)
+//
+//     }
+// }
+
+let i =1
+while(i<=10){
+    if (i%2===0){
+        console.log(i)
+    }
+    i++
+}
