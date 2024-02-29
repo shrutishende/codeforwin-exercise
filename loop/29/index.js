a1f95b3933cd1944e0316077e864664b7829eada
Shruti Shende <shrutishende11@gmail.com>
2024-02-29 14:00:58 +0530

    program to find prime factors of a number.

--- loop/29/index.js
+++ loop/29/index.js
@@ -1,25 +1,25 @@
-let num =10
-let isPrime
-for(let i=2; i<=num; i++)
-{
-    if(num%i===0)
-    {
-        isPrime = 1;
-        for(let j=2; j<=i/2; j++)
-        {
-            if(i%j===0)
-            {
-                isPrime = 0;
-                break;
-            }
-        }
 
-        if(isPrime===1)
-        {
-            console.log(i)
-        }
+
+
+let num = 10
+
+for(let i=2; i<=num; i++){
+
+    if(num % i === 0){
+        //console.log(i)
+        let isPrime = true;
+        for (let j=2; j<i; j++){
+            if(i % j === 0){
+                isPrime=false
+               // console.log(i)
+                break
+            }
     }
 
+        if(isPrime===true){
+            console.log(i)
+        }
 
 
+    }
 }
