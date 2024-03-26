d3e431963d8a6498f49b1dd5605b4fd3e9516c6e
Shruti Shende <shrutishende11@gmail.com>
2024-03-26 14:42:11 +0530

    program to print duplicate elememnt count.

---
+++ array-matrix/13/index.js
@@ -0,0 +1,14 @@
+const arr = [1, 10, 20, 1, 25, 1, 10, 30, 25, 1]
+
+count = 0
+
+for(let i=0; i<arr.length; i++){
+    for(let j=i+1; j<arr.length; j++){
+        if(arr[i]===arr[j]){
+            count++
+            break
+        }
+    }
+}
+
+console.log(count)
