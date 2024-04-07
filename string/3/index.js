e46695551439ba097e36ce3531411b27b60eef75
Shruti Shende <shrutishende11@gmail.com>
2024-04-07 22:43:08 +0530

    program to concatenate two strings Categories.

---
+++ string/3/index.js
@@ -0,0 +1,13 @@
+const str1= "I love"
+const str2="Codeforwin"
+
+let concanatedStr =""
+
+for (let i = 0; i < str1.length; i++) {
+    concanatedStr += str1[i];
+}
+for (let i = 0; i < str2.length; i++) {
+    concanatedStr += str2[i];
+}
+
+console.log(concanatedStr)
